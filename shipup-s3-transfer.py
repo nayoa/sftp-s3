c9c55f487c1333b07a7f902b5640ca0639ce3a68
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-04-09 16:39:14 +0100

    Add try and exceptions to script

--- shipup-s3-transfer.py
+++ shipup-s3-transfer.py
@@ -1,12 +1,18 @@
 import os
 import pysftp
 import sys
-import fnmatch
-import datetime
 import boto3
+import fnmatch
+import shutil
 
-today = datetime.date.today()
-file_pattern = f"*{today}*"
+# Create directory to store CSV reports
+directory = "transfer"
+
+try:
+    os.mkdir(directory)
+    print(directory, "directory created")
+except FileExistsError:
+    print(directory, "directory already exists")
 
 def connect_to_sftp():
 
@@ -22,50 +28,61 @@ def connect_to_sftp():
         
 def get_csv_files(sftp):
 
-    print('Retrieving new csv files')
-    with sftp.cd('reports'):
-        global filenames
-        for filenames in sftp.listdir():
-            if fnmatch.fnmatch(filenames, file_pattern):
-                print(filenames)
-        
-        if sftp.exists(filenames) == True:
-            sftp.get(filenames)
-            print("CSV files copied")
-            upload_file_to_s3(filenames)
-        else:
-            print("New CSV files do not exist")
-            sftp.close()
+    print('Retrieving new csv reports')
 
-
-def upload_file_to_s3(filenames):
-
-    bucketName = os.environ['BUCKET_NAME']
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(filenames, bucketName, filenames)
-
-    print("Copied to S3 Succesfully")
-    return True
-
-
-def archive(sftp, filenames):
-
-    if upload_file_to_s3(filenames):
-        sftp.rename(filenames, archive)
-        print("Move CSV files to Archive")
+    try:
+        sftp.get_d('reports', 'transfer', preserve_mtime=True)
+        print("CSV reports copied")
+        upload_file_to_s3()
+    except ValueError as err:
+        print("Nothing to copy")
+        print(err.args)
         sftp.close()
 
-def cleanup(filenames):
 
-    if os.path.exists(filenames):
-        os.remove(filenames)
+def upload_file_to_s3():
+
+# List of files in directory with CSV reports. 
+# For Loop to upload files in directory and keep original naming convention
+
+    bucket = os.environ['BUCKET_NAME']
+    s3_connect = boto3.client('s3')
+
+    local_path = os.getcwd() # Get current working directory
+
+    try:
+        for filename in os.listdir(directory):
+            file_key_name = filename
+            local_name = local_path + '/' + directory + '/' + filename
+            s3_connect.upload_file(local_name, bucket, file_key_name)
+        print("Copied reports to S3 Succesfully")
+        print(file_key_name)
+        cleanup()
+    except ValueError as err:
+        print("Unable to copy reports to S3")
+        print(err.args)
+
+def cleanup():
+
+    try: 
+        shutil.rmtree(directory)
         print("Clean up complete")
-    else:
+    except ValueError as err:
         print("Nothing to clean up")
+        print(err.args)
 
+
+# def archive(sftp):
+
+#     for filename in sftp.listdir('reports'):
+#         new_path = 'archive' + '/' + filename
+#         sftp.rename(filename, new_path)
+#         print("Move CSV files to Archive")
+#         sftp.close()
+#     else: 
+#         print("Unable to archive")
 
 
 connect_to_sftp()
 get_csv_files(sftp)
-# cleanup(filenames)
-# archive(sftp, filenames)
+# archive(sftp)
