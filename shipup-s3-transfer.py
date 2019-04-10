b6a287b2327216b4c36a2a6fbc8b2742a0c91f8f
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-04-10 11:11:58 +0100

    Amend script with pylint recommendations

--- shipup-s3-transfer.py
+++ shipup-s3-transfer.py
@@ -1,20 +1,25 @@
 import os
-import pysftp
-import sys
-import boto3
-import fnmatch
 import shutil
+import pysftp
+import boto3
 
-# Create directory to store CSV reports
-directory = "transfer"
+def main():
+    """
+    Create directory using variable to temporarily store CSV reports
+    """
+    global directory 
+    directory = "transfer"
 
-try:
-    os.mkdir(directory)
-    print(directory, "directory created")
-except FileExistsError:
-    print(directory, "directory already exists")
+    try:
+        os.mkdir(directory)
+        print(directory, "directory created")
+    except FileExistsError:
+        print(directory, "directory already exists")
 
 def connect_to_sftp():
+    """
+    Connects to sftp server using local environment variables
+    """ 
 
     hostname = os.environ['HOST']
     username = os.environ['USER']
@@ -26,24 +31,31 @@ def connect_to_sftp():
 
     print('Established connection to SFTP server')
         
-def get_csv_files(sftp):
+def get_csv_files(sftp, directory):
+    """
+    Copies the reports from the 'reports' directory on the SFTP server.
+
+    The reports are copied to a local temp folder 'transfer'
+    """
 
     print('Retrieving new csv reports')
 
     try:
         sftp.get_d('reports', 'transfer', preserve_mtime=True)
         print("CSV reports copied")
-        upload_file_to_s3()
+        upload_file_to_s3(directory)
     except ValueError as err:
         print("Nothing to copy")
         print(err.args)
         sftp.close()
 
 
-def upload_file_to_s3():
+def upload_file_to_s3(directory):
+    """
+    List the CSV reports in the temp directory
 
-# List of files in directory with CSV reports. 
-# For Loop to upload files in directory and keep original naming convention
+    Copy all reports to AWS S3
+    """
 
     bucket = os.environ['BUCKET_NAME']
     s3_connect = boto3.client('s3')
@@ -63,6 +75,9 @@ def upload_file_to_s3():
         print(err.args)
 
 def cleanup():
+    """
+    Remove local directory with CSV reports
+    """
 
     try: 
         shutil.rmtree(directory)
@@ -72,7 +87,10 @@ def cleanup():
         print(err.args)
 
 
-# def archive(sftp):
+# def archive(sftp):]
+        """
+        Archive reports copied to S3 on SFTP server
+        """
 
 #     for filename in sftp.listdir('reports'):
 #         new_path = 'archive' + '/' + filename
@@ -84,5 +102,5 @@ def cleanup():
 
 
 connect_to_sftp()
-get_csv_files(sftp)
+get_csv_files(sftp, directory)
 # archive(sftp)
